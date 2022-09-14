4a473fa1eb630ebefbee5969833776885a0a1931
Bar Gabay <bar.gabay.home@gmail.com>
2022-09-14 17:31:23 +0300

    feat; components

--- my-app/src/components/inputs.tsx
+++ my-app/src/components/inputs.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import "./inputsStyles.css";
 
-const Inputs: React.FC = () => {
+class Inputs extends React.Component {
 
-    const submitInfo = () => {
+    constructor(props: any) {
+      super(props);
+      this.firstNameState = {value: ''};
+      this.lastNameState = {value: ''};
+      this.age = {value: ''};
+      this.email = {value: ''};
+      
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    };
-
-    return ( <div>
-        <div id='inputs'>
-            <input type="input" placeholder='First Name'/>
-            <input type="input" placeholder='Last Name'/>
-            <input type="input" placeholder='Email'/>
-            <input type="input" placeholder='Age'/>
-        </div>
-        <button onClick = {submitInfo} type="submit" id='submit'>
-            Submit
-        </button>
-    </div> );
-}
-
-export default Inputs;
+    handleChange(event: any) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event: any) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+        return ( 
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <div id='inputs'>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                    </div>
+                    <input type="submit" value="Submit" id='submit'/>
+                </div> 
+            </form>
+        );
+    }
+  }
